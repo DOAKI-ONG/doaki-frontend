6161310c7de1655f7fdca579ae8d5db8c69386e6
Bealvs <beatrizalves2083@gmail.com>
2025-04-22 21:42:14 -0300

    build: add tailwind-animated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../components/Header"
 import { useState } from "react"
+import {Headset } from "lucide-react"
 
 export function Home (){
     const [isVisible,setIsVisible] = useState(false)
@@ -10,10 +11,10 @@ export function Home (){
     return(
         <div>
             <Header/>
-                <div className="flex flex-col w-screen h-screen items-center justify-center bg-white">
-                    <div className="flex flex-row w-300 h-75 border-3 border-[#619766] rounded-2xl shadow-md ml-5 pr-10 space-x-15 bg-white">
+                <div className="flex flex-col w-screen h-screen items-center justify-center bg-white text-black">
+                    <div className="flex flex-row w-300 h-70 border-3 border-[#619766] rounded-2xl shadow-md ml-5 pr-10 space-x-15 bg-white text-black">
                         <img src="src\assets\logo-1-doaki.png" alt="" className="w-70 h-55 border-gray-100 border-2 rounded-2xl self-center ml-10" />
-                        <div className="flex flex-col items-center justify-center text-justify w-250 bg-white">
+                        <div className="flex flex-col items-center justify-center text-justify w-250 bg-white text-black">
                             <h2 className="text-2xl text-[#619766] mb-5">Nome da ONG</h2>
 
                             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam placeat cupiditate laborum! Possimus nulla minima et nobis impedit neque soluta quidem ipsam quam nihil, id reprehenderit, est aliquam. Quaerat, quo?</p>
@@ -25,8 +26,8 @@ export function Home (){
                             <button type= "submit" className=" w-30 h-12 mt-2 flex justify-center items-center text-white font-bold text-lg rounded-2xl bg-gradient-to-b from-[#B3CC84] to-[#AED59B] self-end hover:opacity-80 transition duration-150 ease-in-out cursor-pointer" >Doar</button>
                         </div>
                     </div>
-                    <a href="" className=" relative w-18 h-18 rounded-full bg-gray-300 self-end right-[30px] bottom-[-110px] shadow-md hover:opacity-80 transition duration-150 ease-in-out cursor-pointer"></a>
-                    <span className="relative flex size-3 self-end right-[35px] bottom-[-40px]">
+                    <button className=" relative w-18 h-18 rounded-full bg-[#F4F1F1] self-end right-[30px] bottom-[-110px] shadow-md hover:opacity-80 transition duration-150 ease-in-out cursor-pointer"> <Headset className="w-10 h-10 text-[#619766] font-[200] relative left-[15px]" style={{ strokeWidth: 1.5 }} /></button>
+                    <span className="relative flex size-3 self-end right-[35px] bottom-[-40px]"> 
                         <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span><span className="relative inline-flex size-3 rounded-full bg-red-500"></span>
                     </span>
                 </div>
