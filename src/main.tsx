f7754d62341e1c8acf1b9c7053c863fb2456ce68
Bealvs <beatrizalves2083@gmail.com>
2025-05-07 16:35:37 -0300

    feat: add LandingPage component and update routing; include new assets and dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {queryClient} from '@/lib/query-client.ts'
 import './index.css'
 import App from './App.tsx'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client = {queryClient}>
+      <App />
+      <Toaster/>
+    </QueryClientProvider>
   </StrictMode>,
 )
